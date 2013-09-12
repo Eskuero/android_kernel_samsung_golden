d78bb0cfcfcd7eba08ba201936de43dc5fb77b4b
OliverG96 <oliverarafo@gmail.com>
2013-09-12 21:28:37 +0100

    bcmdhd: Rework fw and nvram load
    Remove original hack to set fw and nvram paths from kernel module and allow it to be defined from BoardConfig parameter without taking care of weird _b2 extension.
    
    Its used to make a difference between chipset revisions, but since all golden devices comes with b2, it's not needed.
    
    Also take care of dh-harald commit to fix wifi theter:
    https://github.com/dh-harald/android_kernel_samsung_codina/commit/80848dc31c2a42483377388ada1e1c5276a82edb

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -366,7 +366,6 @@ uint dhd_download_fw_on_driverload = TRUE;
  * example nvram_path[MOD_PARAM_PATHLEN]="/projects/wlan/nvram.txt"
  */
 
-/* TODO: Remove hack, we must to be allowed to set parameters for module from BoardConfig define */
 char firmware_path[MOD_PARAM_PATHLEN]="/system/etc/wifi/bcmdhd_sta.bin";
 char nvram_path[MOD_PARAM_PATHLEN]="/system/etc/wifi/nvram_net.txt";
 
@@ -3344,11 +3343,11 @@ dhd_bus_start(dhd_pub_t *dhdp)
 
 	/* try to download image and nvram to the dongle */
 	if  ((dhd->pub.busstate == DHD_BUS_DOWN) &&
-		(firmware_path != NULL) && (firmware_path[0] != '\0') &&
-		(nvram_path != NULL) && (nvram_path[0] != '\0')) {
+		(fw_path != NULL) && (fw_path[0] != '\0') &&
+		(nv_path != NULL) && (nv_path[0] != '\0')) {
 		/* wake lock moved to dhdsdio_download_firmware */
 		if (!(dhd_bus_download_firmware(dhd->pub.bus, dhd->pub.osh,
-		                                firmware_path, nvram_path))) {
+		                                fw_path, nv_path))) {
 			DHD_ERROR(("%s: dhdsdio_probe_download failed. firmware or nvram path is wrong\n",
 			           __FUNCTION__));
 #ifdef DHDTHREAD
